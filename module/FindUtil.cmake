5977efde6c6d6eb1dce88ba409e5de56329ee733
Chenhui.Shi 史晨辉 <190737042@Hytera.com>
2021-04-06 17:57:52 +0800

    fix cmake find_path  bug

--- module/FindUtil.cmake
+++ module/FindUtil.cmake
@@ -1,8 +1,17 @@
-find_path(UTIL_INCLUDES
-        *
-        ${PROJECT_SOURCE_DIR}/utils/*)
+MESSAGE(STATUS "findutil.cmake in")
 
-find_library(UTIL_LIBRARIES NAMES UTIL PATH ${PROJECT_BINARY_DIR}/lib)
+find_path(UTIL_INCLUDES
+         Timer.h
+         ${PROJECT_SOURCE_DIR}/utils)
+
+find_library(UTIL_LIBRARIES NAMES util PATH ${PROJECT_BINARY_DIR}/lib)
+
+if (NOT UTIL_INCLUDES)
+    message("UTIL_INCLUDES not find")
+endif (NOT UTIL_INCLUDES)
+if (NOT UTIL_LIBRARIES)
+    message("UTIL_LIBRARIES not find")
+endif (NOT UTIL_LIBRARIES)
 
 IF(UTIL_INCLUDES AND UTIL_LIBRARIES)
     SET(UTIL_FOUND TRUE)
@@ -14,8 +23,8 @@ IF(UTIL_FOUND)
         MESSAGE(STATUS "foundUTIL:" ${UTIL_INCLUDES})
         MESSAGE(STATUS "foundUTILtlib:" ${UTIL_LIBRARIES})
     ENDIF(NOT UTIL_FIND_REQUIRED)
-ELSE(TEST_FOUND)
+ELSE(UTIL_FOUND)
     IF(UTIL_FIND_REQUIRED)
-        MESSAGE(FATAL_ERROR "could not find hello library")
+        MESSAGE(FATAL_ERROR "could not find util library")
     ENDIF(UTIL_FIND_REQUIRED)
 ENDIF(UTIL_FOUND)
